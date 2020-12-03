6ddeaa3b04493b2c98732f5cf920559c64bdd690
FootLovePeace <soccer.26.promise@gmail.com>
2020-12-03 18:26:28 +0900

    ActiveStorageを導入

--- db/migrate/20201203070327_create_posts.rb
+++ db/migrate/20201203070327_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title,           null: false
-      t.text :text,              null: false
-      t.integer :country_id,      null: false
-      t.integer :genre_id,        null: false
+      t.string :title,       null: false
+      t.text :text,          null: false
+      t.integer :country_id, null: false
+      t.integer :genre_id,   null: false
       t.references :user, foreign_key: true          
       t.timestamps
     end
