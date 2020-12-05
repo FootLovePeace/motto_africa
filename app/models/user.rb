0cc0e73b1df04f08a9ebe6a1976b41afc6fb3099
FootLovePeace <soccer.26.promise@gmail.com>
2020-12-05 13:25:01 +0900

    各モデルのアソシエーションを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts       
-
+  has_many :comments
  
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
