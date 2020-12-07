5c1c1d0c81fddb5f70aa1c0606b264e3f90666a6
FootLovePeace <soccer.26.promise@gmail.com>
2020-12-07 20:20:15 +0900

    トップページのレイアウトを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_post, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
 
   def index  
     @posts = Post.includes(:user).order("created_at DESC")
@@ -19,17 +21,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
   end
 
-  def edit
-    @post = Post.find(params[:id])    
+  def edit    
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(params[:id])
     else
@@ -48,5 +47,15 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :text, :country_id, :genre_id, :image).merge(user_id: current_user.id)  
   end  
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @post.user_id
+      redirect_to action: :index
+    end
+  end
 
 end
