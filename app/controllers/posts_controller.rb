15afd92f735caa9ff0f8d3bf09cc452df19172eb
FootLovePeace <soccer.26.promise@gmail.com>
2020-12-09 16:24:24 +0900

    トップページのレイアウトを編集（2）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :new, :create, :destroy]
 
   def index  
-    @posts = Post.includes(:user).order("created_at DESC")
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new 
