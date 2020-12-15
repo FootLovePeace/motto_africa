adf0896b539674b23229076ff9cfa2c8a06697b2
FootLovePeace <soccer.26.promise@gmail.com>
2020-12-15 20:11:40 +0900

    userモデルの単体テストコードの編集（2）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 4)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"0000aaa"}
     password_confirmation {password}
   end
 end
